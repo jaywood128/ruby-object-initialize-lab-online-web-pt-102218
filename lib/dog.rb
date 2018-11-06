15eae0f9d56a8b4cbffb31bc25342ff591fca93f
Johnathon Wood <jaywood128@gmail.com>
2018-11-06 19:15:37 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -7,7 +7,7 @@ class Dog
   def initialize(name)
     @name = name
   end
-  # def breed(breed = "Mutt")
-  #   @breed = breed
-  # end
+   def breed(breed = "Mutt")
+     @breed = breed
+   end
 end
